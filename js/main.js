054d4879ef0bcbbf6b7ec5d2dcf5ad53373f1a01
Klaus <839887585@qq.com>
2017-09-04 22:04:48 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(window).load(function() {
-    $('.flexslider').flexslider({
-    	animation: "slide",
-    	controlsContainer: $(".carousel-controls"),
-    	customDirectionNav: $(".carousel-arrows a")
-    });
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlsContainer: $(".carousel-controls"),
+		customDirectionNav: $(".carousel-arrows a")
+	});
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		$('.carousel-arrows').css('display','none');
+	$(window).resize(function() {
+		if ($('.flexslider').width() < 980) {
+			$('.carousel-arrows').css('display', 'none');
+		} else {
+			$('.carousel-arrows').css('display', 'block');
+		}
+	});
+
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		$('.carousel-arrows').css('display', 'none');
+	}
+
+	if ($('.flexslider').width() < 980) {
+		$('.carousel-arrows').css('display', 'none');
 	}
 });
