9a63acd8521cf8da4418dbfcc80c3ac426583f5e
Klaus <839887585@qq.com>
2017-09-15 15:12:20 +0800

    1

---
+++ js/overlayPage.js
@@ -0,0 +1,14 @@
+/* header.js
+ * author:Klaus
+ * 2017/9/15
+ */
+(function() {
+
+	$('#driveTest').click(function() {
+		$('.overlay-page').show();
+	});
+
+	$('.overlay-page').click(function() {
+		$('.overlay-page').hide();
+	});
+})();
